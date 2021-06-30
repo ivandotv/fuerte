35a3f69133d80545fcb9f5a81bdf191971191b61
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-06-30 13:13:32 +0200

    init "transport-idb" package

---
+++ packages/transport-idb/jest.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires  */
+const baseConfig = require('../../jest.config')
+
+module.exports = {
+  ...baseConfig,
+  rootDir: '.',
+  projects: undefined,
+  testMatch: ['<rootDir>/src/**/__tests__/?(*.)+(spec|test).[jt]s?(x)'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**',
+    '!<rootDir>/src/index.ts',
+    '!<rootDir>/src/__tests__/**',
+    '!<rootDir>/src/scratch/**',
+    '!<rootDir>/src/**/__tests__/**',
+    '!<rootDir>/src/**/__fixtures__/**'
+  ]
+}
