3459afc953db57b299a15185673baef6553db975
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-06-30 21:25:11 +0200

    setup all methods

---
+++ packages/transport-idb/src/idb-transport.ts
@@ -0,0 +1,97 @@
+import { Model, Transport } from '@fuerte/core'
+import { IDBPDatabase, openDB } from 'idb'
+
+// type DBValue = { station: RadioStation; date: Date; _id: string }
+
+// interface LocalSchema<T extends Model, K extends string> extends DBSchema {
+//   [K]: {
+//     key: string
+//     value: T
+//   }
+// }
+
+export class TransportIDB<T extends Model> implements Transport<T> {
+  protected db!: IDBPDatabase
+
+  //TODO - clear database
+  constructor(
+    public dbName: string,
+    public store = 'models',
+    public keyPath = 'identityKey',
+    protected customInitFn?: (
+      dbName: string,
+      store: string,
+      keyPath: string
+    ) => Promise<IDBPDatabase>
+  ) {}
+
+  async load(): Promise<{ data: any[] }> {
+    const db = await this.getDB()
+    const data = await db.getAll(this.store)
+
+    return { data }
+  }
+
+  async save(model: T) {
+    const db = await this.getDB()
+
+    await db.put(this.store, model)
+  }
+
+  async delete(model: T) {
+    const db = await this.getDB()
+
+    await db.delete(this.store, model.identity)
+  }
+
+  async deleteAll() {
+    const db = await this.getDB()
+
+    await db.clear(this.store)
+  }
+
+  async reload(model: T) {
+    const db = await this.getDB()
+
+    return db.get(this.store, model.identity)
+  }
+
+  async initDB() {
+    const db = await openDB(this.dbName, 1, {
+      upgrade: (db, oldVersion) => {
+        if (oldVersion === 0) {
+          db.createObjectStore(this.store, { keyPath: this.keyPath })
+        }
+      },
+      blocked() {
+        if (__DEV__) {
+          console.warn('idb: blocked')
+        }
+      },
+      blocking() {
+        if (__DEV__) {
+          console.warn('idb: blocking')
+        }
+      },
+      terminated() {
+        if (__DEV__) {
+          console.warn('idb: terminated')
+        }
+      }
+    })
+
+    return db
+  }
+
+  async getDB() {
+    if (!this.db) {
+      if (this.customInitFn) {
+        this.db = await this.customInitFn(this.dbName, this.store, this.keyPath)
+      } else {
+        this.db = await this.initDB()
+      }
+    }
+
+    return this.db
+  }
+}
