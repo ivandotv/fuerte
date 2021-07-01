c455a1492d45e1d2e67113d54973edc430c2a1f4
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-07-01 21:22:00 +0200

    core: add getById to transport interface

--- packages/core/src/transport/transport.ts
+++ packages/core/src/transport/transport.ts
@@ -3,6 +3,8 @@ import { Model } from '../model/Model'
 export interface Transport<TModel extends Model<any> = Model<any>> {
   load(config?: any): Promise<{ data: any[] }>
 
+  getById(id: string): Promise<{ data?: any } | void>
+
   save(model: TModel, config?: any): Promise<{ data?: any } | void>
 
   delete(model: TModel, config?: any): Promise<{ data?: any } | void>
@@ -13,6 +15,10 @@ export class InMemoryTransport<TModel extends Model<any> = Model<any>>
 {
   load(): Promise<{ data: any[] }> {
     return Promise.resolve({ data: [] })
+  }
+
+  getById(id: string): Promise<void> {
+    return Promise.resolve()
   }
 
   save(model: TModel): Promise<void> {
