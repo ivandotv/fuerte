d82ee3f96ba9a22fae5d582cc7461fed948e777b
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-09-20 14:09:49 +0200

    remove getById method from transport interface

--- packages/core/src/transport/transport.ts
+++ packages/core/src/transport/transport.ts
@@ -3,8 +3,6 @@ import type { Model } from '../model/Model'
 export interface Transport<TModel extends Model<any> = Model<any>, TDTO = any> {
   load(config?: any): Promise<{ data: TDTO[] }>
 
-  getById(id: string): Promise<{ data?: TDTO } | void>
-
   save(model: TModel, config?: any): Promise<{ data?: any } | void>
 
   delete(model: TModel, config?: any): Promise<{ data?: any } | void>
@@ -15,10 +13,6 @@ export class StubTransport<TModel extends Model<any> = Model<any>>
 {
   load(): Promise<{ data: any[] }> {
     return Promise.resolve({ data: [] })
-  }
-
-  getById(id: string): Promise<void> {
-    return Promise.resolve()
   }
 
   save(m: TModel): Promise<void> {
