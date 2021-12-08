757e41ea91b30db2eb5bf4d74d66c12ff12aa51c
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-12-08 16:10:33 +0100

    remove core dist

--- packages/core/dist/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './model/Model';
-export * from './collection/Collection';
-export * from './transport/transport';
-export { ASYNC_STATUS, unwrapResult } from './utils/utils';
-export * from './utils/types';
--- packages/core/dist/types/model/identity-error.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class IdentityError extends Error {
-    constructor(message: string);
-}
