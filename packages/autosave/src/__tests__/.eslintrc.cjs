c416cdad8691d931451c3b4f1af8bb4975ce9c12
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2022-06-17 12:13:45 +0200

    remove autosave package

--- packages/autosave/src/__tests__/.eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: '../../.eslintrc.cjs',
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
-  }
-}
